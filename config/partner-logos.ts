4acf3fb601414e1524212148ab3e6dcb023d47ea
StiviMoon <johanprogamer01@gmail.com>
2026-02-07 20:52:13 -0500

    Add Partner Logos and Remove Unused Image Optimization Scripts
    - Added new partner logos for Land Trust Partners Of Florida LLC and BUI to partner-logos.ts.
    - Removed several unused image optimization scripts to streamline the codebase and improve maintainability.

--- config/partner-logos.ts
+++ config/partner-logos.ts
@@ -29,4 +29,18 @@ export const PARTNER_LOGOS: LogoItem[] = [
     width: 150,
     height: 150,
   },
+  {
+    src: "/LTP.png",
+    alt: "Land Trust Partners Of Florida LLC",
+    href: "#",
+    width: 200,
+    height: 60,
+  },
+  {
+    src: "/bui.svg",
+    alt: "BUI",
+    href: "#",
+    width: 200,
+    height: 60,
+  },
 ]
