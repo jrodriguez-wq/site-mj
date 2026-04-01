01754d5bd9109ce5dadc6d625f3357ce0c610a48
StiviMoon <johanprogamer01@gmail.com>
2026-04-01 10:53:13 -0500

    Add Facebook Pixel for Enhanced Tracking and Analytics
    - Integrated Facebook Pixel script for improved tracking of page views and user interactions.
    - Added DNS prefetching for Facebook's resources to optimize loading times.
    - Included a noscript fallback for users with JavaScript disabled, ensuring tracking continuity.

--- app/layout.tsx
+++ app/layout.tsx
@@ -114,6 +114,7 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://js.hsforms.net" />
         <link rel="dns-prefetch" href="https://static.hsappstatic.net" />
         <link rel="dns-prefetch" href="https://meetings.hubspot.com" />
+        <link rel="dns-prefetch" href="https://connect.facebook.net" />
 
         {/* ── Preload LCP hero image ─────────────────────────────────────────── */}
         <link
@@ -142,11 +143,40 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Meta (Facebook) Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1266988268208353');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${outfit.variable} ${dmSans.variable} ${pacifico.variable}`}
         suppressHydrationWarning
       >
+        <noscript>
+          <img
+            height={1}
+            width={1}
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1266988268208353&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <StructuredDataComponent data={structuredData} />
         <ConditionalSiteLayout>{children}</ConditionalSiteLayout>
         <Analytics />
